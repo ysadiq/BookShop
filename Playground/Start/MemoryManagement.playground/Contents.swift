83ead8ba4f78379c3a2d9e1b2bebe29258ee066b
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2021-01-20 10:46:40 +0200

    Update Contents.swift

--- Playground/Start/MemoryManagement.playground/Contents.swift
+++ Playground/Start/MemoryManagement.playground/Contents.swift
@@ -3,9 +3,10 @@ do {
     let book = Book(title: "Dialogue with an atheist",
                     author: author)
 
-    let editor = Editor(name: "M. I. Waley")
-    author.books.append(book)
 
+    let editor = Editor(name: "M. I. Waley")
     book.editor = editor
     editor.books.append(book)
+
+    author.books.append(book)
 }
